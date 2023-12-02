a192859ea2076e3f89c8125ae3de0d0db0aed42d
kbw <ktd1595@ryencatchers.com>
2023-12-02 15:20:10 +0900

    Initial commit

---
+++ src/main/java/com/ck/ck/CkApplication.java
@@ -0,0 +1,13 @@
+package com.ck.ck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CkApplication.class, args);
+  }
+
+}
